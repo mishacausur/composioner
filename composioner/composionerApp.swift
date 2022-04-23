9ab5f7d83f333a2d8afbca7e2369376b50072eb1
Misha Causur <mishacausur@mail.ru>
2022-04-24 00:34:47 +0500

    Initial Commit

---
+++ composioner/composionerApp.swift
@@ -0,0 +1,17 @@
+//
+//  composionerApp.swift
+//  composioner
+//
+//  Created by Misha Causur on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct composionerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
