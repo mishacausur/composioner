765b53d7a825ab66a2d94beaafa3909a2d5104d4
Misha Causur <mishacausur@mail.ru>
2022-05-01 15:55:04 +0500

    first task is done

--- composioner/Thinker Managers/DownloadImageAsync.swift
+++ composioner/Thinker Managers/DownloadImageAsync.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+class DownloadImageAsyncImageLoader {
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                let image = UIImage(data: data),
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                completionHandler(nil, error)
+                return
+            }
+            completionHandler(image, nil)
+        }.resume()
+    }
+}
+
 class DownloadImageAsyncViewModel: ObservableObject {
     @Published var image: UIImage? = nil
     
