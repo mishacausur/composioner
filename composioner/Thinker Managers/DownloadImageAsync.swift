2bf2d05ae5214db666354917f9c00ff12eeca3f7
Misha Causur <mishacausur@mail.ru>
2022-05-01 15:40:54 +0500

    thinker is implemented

---
+++ composioner/Thinker Managers/DownloadImageAsync.swift
@@ -0,0 +1,22 @@
+//
+//  DownloadImageAsync.swift
+//  composioner
+//
+//  Created by Misha Causur on 01.05.2022.
+//
+
+import SwiftUI
+
+class DownloadImageAsyncViewModel
+
+struct DownloadImageAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImageAsync()
+    }
+}
