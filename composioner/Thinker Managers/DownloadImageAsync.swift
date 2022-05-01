69933c0c25e2abcec42d764f5fede40343841964
Misha Causur <mishacausur@mail.ru>
2022-05-01 16:01:27 +0500

    method is separated

--- composioner/Thinker Managers/DownloadImageAsync.swift
+++ composioner/Thinker Managers/DownloadImageAsync.swift
@@ -10,26 +10,34 @@ import SwiftUI
 class DownloadImageAsyncImageLoader {
     let url = URL(string: "https://picsum.photos/200")!
     
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+    
     func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let data = data,
-                let image = UIImage(data: data),
-                let response = response as? HTTPURLResponse,
-                response.statusCode >= 200 && response.statusCode < 300 else {
-                completionHandler(nil, error)
-                return
-            }
-            completionHandler(image, nil)
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, error)
         }.resume()
     }
 }
 
 class DownloadImageAsyncViewModel: ObservableObject {
     @Published var image: UIImage? = nil
-    
+    let loader = DownloadImageAsyncImageLoader()
     func fetchImage() {
-        self.image = UIImage(systemName: "heart.fill")
+        loader.downloadWithEscaping { [weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
     }
 }
 
