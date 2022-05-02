917c70c06afed3f1b7f1cf2979b93788d1617253
Misha Causur <mishacausur@mail.ru>
2022-05-03 01:14:23 +0500

    asyncer in progress

--- composioner/Thinker Managers/Asyncer.swift
+++ composioner/Thinker Managers/Asyncer.swift
@@ -6,3 +6,24 @@
 //
 import SwiftUI
 import Combine
+
+class AsyncerViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+}
+
+struct Asyncer: View {
+@StateObject private var viewModel = AsyncerViewModel()
+    var body: some View {
+        List {
+            ForEach(viewModel.dataArray, id: \.self) { data in
+                Text(data)
+            }
+        }
+    }
+}
+
+struct Asyncer_Previews: PreviewProvider {
+    static var previews: some View {
+        Asyncer()
+    }
+}
