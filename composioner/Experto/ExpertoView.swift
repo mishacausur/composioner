7c8196a305f5807f170b4691cbcb7a0d209b8438
Misha Causur <mishacausur@mail.ru>
2022-05-06 15:50:27 +0500

    refactored

--- composioner/Experto/ExpertoView.swift
+++ composioner/Experto/ExpertoView.swift
@@ -12,16 +12,31 @@ struct ExpertoView: View {
         NavigationView {
             ScrollView {
                 HStack {
+                    VStack(alignment: .leading) {
+                        Text("Create your room")
+                            .foregroundColor(.white)
+                            .font(.system(size: 18).bold())
+                        Text("Use to chat and more")
+                            .foregroundColor(.white)
+                            .font(.system(size: 14))
+                            .padding(.top, 8)
+                        Capsule(style: .circular)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 42)
+                            .foregroundColor(.white)
+                            .padding(.top, 8)
+                    }
+                    .padding()
                     ZStack {
                         Circle()
-                            .stroke(.white, lineWidth: 14)
-                            .padding()
+                            .stroke(.white, lineWidth: 12)
+                            
                             .rotationEffect(Angle(degrees: 270))
                             .opacity(0.4)
                         Circle()
                             .trim(from: 0, to: 0.74)
-                            .stroke(style: StrokeStyle(lineWidth: 14.0, lineCap: .round, lineJoin: .round))
-                            .padding()
+                            .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                            
                             .foregroundColor(.white)
                             .rotationEffect(Angle(degrees: 270))
                         Image(systemName: "checkmark")
@@ -30,14 +45,10 @@ struct ExpertoView: View {
                         .frame(width: 44, height: 44)
                         .padding()
                     }
-                    .frame(width: 140, height: 140)
-                    VStack {
-                        Text("Your score today is 208")
-                            .foregroundColor(.white)
-                            .font(.body)
-                    }
+                    .frame(width: 120, height: 120)
+                    .padding(.trailing, 16)
                 }
-                .frame(height: 200)
+                .frame(height: 160)
                 .frame(maxWidth: .infinity)
                 .background(.purple)
                 .cornerRadius(16)
