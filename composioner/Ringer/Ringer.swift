4537b2b018a12f5d68d7a7148aec36f1cf441764
Misha Causur <mishacausur@mail.ru>
2022-05-08 16:20:31 +0500

    rings are done

--- composioner/Ringer/Ringer.swift
+++ composioner/Ringer/Ringer.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct Ringer: View {
+    @State var percentage1: Double = 0
+    @State var percentage2: Double = 0
+    @State var percentage3: Double = 0
     var body: some View {
         ZStack {
-            Ring(lineWidth: 50, backgroundColor: .red.opacity(0.2), foregroundColor: .red, percentage: 70)
+            Ring(lineWidth: 30, backgroundColor: .cyan.opacity(0.2), foregroundColor: .cyan, percentage: percentage1)
+                .frame(width: 140, height: 140)
+                .onTapGesture {
+                    percentage1 = 80
+                }
+            Ring(lineWidth: 30, backgroundColor: .green.opacity(0.2), foregroundColor: .green, percentage: percentage2)
+                .frame(width: 220, height: 220)
+                .onTapGesture {
+                    percentage2 = 100
+                }
+            Ring(lineWidth: 30, backgroundColor: .red.opacity(0.2), foregroundColor: .red, percentage: percentage3)
                 .frame(width: 300, height: 300)
+                .onTapGesture {
+                    percentage3 = 65
+                }
         }
     }
 }
