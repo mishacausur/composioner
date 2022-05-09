f1a6979066bb9725f6fd1f7670ae630bcbf120e1
Misha Causur <mishacausur@mail.ru>
2022-05-09 21:11:25 +0500

    asynclet is implemented

---
+++ composioner/Thinker Managers/AsyncLet.swift
@@ -0,0 +1,22 @@
+//
+//  AsyncLet.swift
+//  composioner
+//
+//  Created by Misha Causur on 09.05.2022.
+//
+
+import SwiftUI
+
+struct AsyncLet: View {
+    
+    @State private var images: [UIImage] = []
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AsyncLet_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncLet()
+    }
+}
