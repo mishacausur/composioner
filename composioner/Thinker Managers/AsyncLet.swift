b70d0dd06bce3374240009fd1a1c3429e301592e
Misha Causur <mishacausur@mail.ru>
2022-05-09 21:23:25 +0500

    first part is ready

--- composioner/Thinker Managers/AsyncLet.swift
+++ composioner/Thinker Managers/AsyncLet.swift
@@ -10,8 +10,55 @@ import SwiftUI
 struct AsyncLet: View {
     
     @State private var images: [UIImage] = []
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let url = URL(string: "https://picsum.photos/300")!
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("Async Let")
+            .onAppear {
+               Task {
+                   do {
+                       let image = try await fetchImage()
+                       self.images.append(image)
+                       
+                       let image1 = try await fetchImage()
+                       self.images.append(image1)
+                       
+                       let image2 = try await fetchImage()
+                       self.images.append(image2)
+                       
+                       let image3 = try await fetchImage()
+                       self.images.append(image3)
+                       
+                   } catch {
+                       
+                   }
+               }
+            }
+        }
+    }
+    
+    func fetchImage() async throws -> UIImage {
+        do {
+            let response = try await URLSession.shared.data(from: url)
+            if let image = UIImage(data: response.0) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
     }
 }
 
