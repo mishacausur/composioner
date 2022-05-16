c8263d2f8f5b77fd38aa857c7b7bd6139c4df7a7
Misha Causur <mishacausur@mail.ru>
2022-05-16 23:50:12 +0500

    in progress

---
+++ composioner/Continuations/ContinuationView.swift
@@ -0,0 +1,26 @@
+//
+//  ContinuationView.swift
+//  composioner
+//
+//  Created by Misha Causur on 16.05.2022.
+//
+
+import SwiftUI
+
+final class ContinuationViewModel: ObservableObject {
+    
+}
+
+struct ContinuationView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct ContinuationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContinuationView()
+    }
+}
