b403432489918d9b13dd6db756cc310ec8fe3453
Misha Causur <mishacausur@mail.ru>
2022-05-17 00:10:04 +0500

    vm is implemented

--- composioner/Continuations/ContinuationView.swift
+++ composioner/Continuations/ContinuationView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+final class CVMNetworkManager {
+    func getData(with url: URL) async throws -> Data {
+        do {
+            let response = try await URLSession.shared.data(from: url, delegate: nil)
+            return response.0
+        } catch {
+            throw error
+        }
+    }
+}
+
 final class ContinuationViewModel: ObservableObject {
     @Published var image: UIImage? = nil
+    let networker = CVMNetworkManager()
     func getImages() async {
-        
+        guard let url = URL(string: "https://picsum.photos/200") else { return }
+        do {
+            let data = try await networker.getData(with: url)
+            if let image = UIImage(data: data) {
+                await MainActor.run(body: {
+                    self.image = image
+                })
+            }
+        } catch {
+            error
+        }
     }
 }
 
