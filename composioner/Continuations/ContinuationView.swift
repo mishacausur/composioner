8e5da7c9f5c8955e4891f8c10300b5fdc021ba07
Misha Causur <mishacausur@mail.ru>
2022-05-16 23:51:46 +0500

    in progress still

--- composioner/Continuations/ContinuationView.swift
+++ composioner/Continuations/ContinuationView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 final class ContinuationViewModel: ObservableObject {
-    
+    @Published var image: UIImage? = nil
 }
 
 struct ContinuationView: View {
+    @StateObject private var viewModel = ContinuationViewModel()
     var body: some View {
         ZStack {
-            
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
         }
     }
 }
