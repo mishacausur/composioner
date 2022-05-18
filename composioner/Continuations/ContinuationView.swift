5c8f0849d70c08db9f09c8f3ddcf3591994722c1
Misha Causur <mishacausur@mail.ru>
2022-05-18 23:46:07 +0500

    viewmodel is separated

--- composioner/Continuations/ContinuationView.swift
+++ composioner/Continuations/ContinuationView.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-final class ContinuationViewModel: ObservableObject {
-    @Published var image: UIImage? = nil
-    let networker = CVMNetworkManager()
-    func getImages() async {
-        guard let url = URL(string: "https://picsum.photos/200") else { return }
-        do {
-            let data = try await networker.getDataCont(with: url)
-            if let image = UIImage(data: data) {
-                await MainActor.run(body: {
-                    self.image = image
-                })
-            }
-        } catch {
-            print(error)
-        }
-    }
-    
-    func getHeart() {
-        networker.getHeart { [weak self] image in
-            self?.image = image
-        }
-    }
-    func heartAsync() async {
-        self.image = await networker.asycHeart()
-    }
-}
-
 struct ContinuationView: View {
     @StateObject private var viewModel = ContinuationViewModel()
     var body: some View {
---
+++ composioner/Continuations/ContinuationViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ContinuationViewModel.swift
+//  composioner
+//
+//  Created by Misha Causur on 18.05.2022.
+//
+
+import Foundation
+
+final class ContinuationViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    let networker = CVMNetworkManager()
+    func getImages() async {
+        guard let url = URL(string: "https://picsum.photos/200") else { return }
+        do {
+            let data = try await networker.getDataCont(with: url)
+            if let image = UIImage(data: data) {
+                await MainActor.run(body: {
+                    self.image = image
+                })
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getHeart() {
+        networker.getHeart { [weak self] image in
+            self?.image = image
+        }
+    }
+    func heartAsync() async {
+        self.image = await networker.asycHeart()
+    }
+}
