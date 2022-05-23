2842f8b127681e503ce7bd674f2e8a8bede8de78
Misha Causur <mishacausur@mail.ru>
2022-05-23 23:31:05 +0500

    actors in progress

--- composioner/Actors/Actors.swift
+++ composioner/Actors/Actors.swift
@@ -7,17 +7,72 @@
 
 import SwiftUI
 
+class DataClass {
+    static let instance = DataClass()
+    private init() { }
+    
+    var data: [String] = []
+    
+    func randomData() -> String? {
+        self.data.append(UUID().uuidString)
+        return data.randomElement()
+    }
+}
 
+struct HomeView: View {
+    let manager = DataClass.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    var body: some View {
+        ZStack {
+            Color.mint.opacity(0.6).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            DispatchQueue.global(qos: .background).async {
+                if let data = manager.randomData() {
+                    DispatchQueue.main.async {
+                        self.text = data
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct FavouriteView: View {
+    let manager = DataClass.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    var body: some View {
+        ZStack {
+            Color.pink.opacity(0.6).ignoresSafeArea()
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            DispatchQueue.global(qos: .background).async {
+                if let data = manager.randomData() {
+                    DispatchQueue.main.async {
+                        self.text = data
+                    }
+                }
+            }
+        }
+    }
+}
 
 struct Actors: View {
     var body: some View {
         TabView {
-            Text("Hi")
+            HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house.fill")
                 }
             
-            Text("Hi again")
+            FavouriteView()
                 .tabItem {
                     Label("Favourites", systemImage: "heart.fill")
                 }
