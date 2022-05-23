fd835f9f8a7935c366ec7173d490572e5f54163d
Misha Causur <mishacausur@mail.ru>
2022-05-23 23:13:43 +0500

    actors file is added

---
+++ composioner/Actors/Actors.swift
@@ -0,0 +1,20 @@
+//
+//  Actors.swift
+//  composioner
+//
+//  Created by Misha Causur on 23.05.2022.
+//
+
+import SwiftUI
+
+struct Actors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Actors_Previews: PreviewProvider {
+    static var previews: some View {
+        Actors()
+    }
+}
