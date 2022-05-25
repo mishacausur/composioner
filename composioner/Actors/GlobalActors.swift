7f12274b08349dcc68a626bae58fa283c217f8ae
Misha Causur <mishacausur@mail.ru>
2022-05-25 23:06:06 +0500

    GA in progress

--- composioner/Actors/GlobalActors.swift
+++ composioner/Actors/GlobalActors.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 
+final class GlobalActorViewModel: ObservableObject {
+    @Published var data: [String] = []
+}
+
 struct GlobalActors: View {
+    @StateObject private var viewModel = GlobalActorViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                ForEach(viewModel.data, id: \.self) {
+                    Text($0)
+                        .font(.headline)
+                }
+            }
+        }
     }
 }
 
