073981090a9405d0a47ac791f1ebddf9ccab99d1
Misha Causur <mishacausur@mail.ru>
2022-05-25 23:03:29 +0500

    global actors are started

---
+++ composioner/Actors/GlobalActors.swift
@@ -0,0 +1,20 @@
+//
+//  GlobalActors.swift
+//  composioner
+//
+//  Created by Misha Causur on 25.05.2022.
+//
+
+import SwiftUI
+
+struct GlobalActors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GlobalActors_Previews: PreviewProvider {
+    static var previews: some View {
+        GlobalActors()
+    }
+}
