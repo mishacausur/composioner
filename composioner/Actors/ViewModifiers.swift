006246ca86addf2f8a74ff05bac1ec72c664c50d
Misha Causur <mishacausur@mail.ru>
2022-05-28 23:27:37 +0500

    viewmodifiers are implemented

---
+++ composioner/Actors/ViewModifiers.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModifiers.swift
+//  composioner
+//
+//  Created by Misha Causur on 28.05.2022.
+//
+
+import SwiftUI
+
+struct ViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModifiers_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModifiers()
+    }
+}
