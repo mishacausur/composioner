d7d561f06e5f2e4114215d8ad65546e83ba616ba
Misha Causur <mishacausur@mail.ru>
2022-05-31 23:29:28 +0500

    sendable is added

---
+++ composioner/Sendable/Sendable.swift
@@ -0,0 +1,20 @@
+//
+//  Sendable.swift
+//  composioner
+//
+//  Created by Misha Causur on 31.05.2022.
+//
+
+import SwiftUI
+
+struct Sendable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sendable_Previews: PreviewProvider {
+    static var previews: some View {
+        Sendable()
+    }
+}
