5d324a4d6063730b01a110ed1f7f4df59d279e55
Misha Causur <mishacausur@mail.ru>
2022-06-02 00:52:30 +0500

    AsyncPublisher section is implemented

---
+++ composioner/AsyncPublisher/AsyncPublisher.swift
@@ -0,0 +1,20 @@
+//
+//  AsyncPublisher.swift
+//  composioner
+//
+//  Created by Misha Causur on 02.06.2022.
+//
+
+import SwiftUI
+
+struct AsyncPublisher: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AsyncPublisher_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncPublisher()
+    }
+}
